6d3c8b481e4827a815e011b325c225c18428b3c4
nastyapopova22 <89507790719@mail.ru>
2023-08-15 16:30:06 +0300

    hw 5

--- hw N+5.js
+++ hw N+5.js
@@ -2,9 +2,9 @@
 
 function min (a, b) {
     if (a < b) {
-        return (a);
+        return a;
     } else  {
-        return (b);
+        return b;
     }
 }
 console.log(min(10,5));
@@ -22,26 +22,31 @@ console.log(comparison(8));
 //задание 3
 
  function square(c) {
-   return (c*c);
-
+   if (c**2) {
+    console.log(square);
+   }
  }
- console.log(square(7));
+ 
+ function square(p) {
+    return (p**p);
+ 
+  }
 
  //задание 4
 
  function age() {
-    let answer = (prompt('Сколько Вам лет?'));
+    let answer = prompt('Сколько Вам лет?');
 
     if (isNaN(answer) || answer === "" || answer === null) {
-        return('не число');
+        return alert('не число');
     } else if (answer < 0 ) {
         return ('Вы ввели неправильное значение');
 
     } else if (answer >= 0 && answer <= 12) {
-        return ('Привет, друг!');
+        return alert('Привет, друг!');
         
     } else (answer >= 13) 
-    return ('Добро пожаловать!');
+    return alert('Добро пожаловать!');
     }
 age();
 
@@ -49,20 +54,19 @@ age();
 const type = (n, d) => {
     if (isNaN (Number(n)) || isNaN (Number(d))) {
         return (' Одно или оба значения не являются числом');
-    } else (Number(n)) ||(Number(d))
+    } else 
         return (n * d);
     }
 
 
 //задание 6
 function example () {
-    let cube = Number(prompt('Введите число'));
-    if (Number(cube) ** 3) {
-        alert(`${example} в кубе равняется ${cube} `);
+    let cube = prompt('Введите число');
+    if (cube ** 3) {
+        alert(`n в кубе равняется ${cube} `);
     } else 
         alert ('Переданный параметр не является числом');
     }
-    example();
 
 
 //задание 7
@@ -95,21 +99,3 @@ function getRectangleArea() {
   console.log(circle2.getArea());
   console.log(circle2.getPerimeter());
 
-
-  //задание 8
-
-  function season() {
-    let month = Number(prompt( 'Введите номер месяца'));
-    if (month === 1 && month === 2 || month === 12) {
-        return alert('Зима');
-    } else if (month >= 3 && month <= 5){
-        return alert('Весна');
-    } else if (month >= 6 && month <= 8){
-        return alert('Лето');
-    } else if (month >= 9 && month <= 11){
-        return alert('Осень');
-    } else {
-        return alert('Вы ввели неверное значение');
-    }
-}
-season();
