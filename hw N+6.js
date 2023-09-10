c685e5d75e523f2ab0eed4ae0e56d7ef1b0f68c4
nastyapopova22 <89507790719@mail.ru>
2023-09-10 12:01:38 +0300

    hw-8

--- hw N+6.js
+++ hw N+6.js
@@ -24,7 +24,7 @@ for (let i = 0; i < 3; i++) {
 	arr[i] = []; 
 	
 	for (let j = 0; j < 3; j++) {
-		arr[j].push(1); 
+		arr[i].push(1); 
 	}
     
 }
--- hw N+8.js
+++ hw N+8.js
@@ -10,10 +10,6 @@ const people = [
     if (a.age > b.age) {
       return 1;
     }
-    if (a.age < b.age) {
-      return -1;
-    }
-    return 0;
   });
  
  console.log(people.sort());
@@ -21,53 +17,30 @@ const people = [
   
 //задание 2
 
-function isPositive(number) {
-    if (Number(number) > 0) {
+function isPositive(filter) {
+    if (filter > 0)
         return true;
-    } else { 
-        return false; 
     }
+    function isMale(filter) {
+   if (filter.gender === 'male')
+          return true;
     }
+    function filter(arr,ruleFunction) {
+        let output = [];
 
-    function isMale(gender) {
-   if (gender === 'male')
-          return true
-    else 
-          return false;
+        for (let i = 0; i < arr.length; i++) {
+           if (ruleFunction (arr[i])) {
+            output.push(arr[i]);
+        }
     }
-
-    function filter(arr, ruleFunc) {
-        let funcArr = [];
-
-        for (let x = 0; x < arr.length; x++) {
-           if (typeof arr [x] === 'object') {
-               if (ruleFunc(arr[x].gender)) {
-                funcArr.push(arr[x]);
-               }
-            } 
-               else (ruleFunc(arr[x])); {
-               funcArr.push(arr[x]);}
-    
-        }       
+    return output;
 }
+console.log(filter([3, -4, 1, 9], isPositive));
+console.log(filter(people, isMale)); 
+    
+    
+// задание 3
 
-    
-    console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
-    
-   //people = [
-  //     {name: 'Глеб', gender: 'male'},
- //      {name: 'Анна', gender: 'female'},
-  //     {name: 'Олег', gender: 'male'},
- //      {name: 'Оксана', gender: 'female'}
-  //  ];
-    
-    console.log(filter(people, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
-    
-    
-    // задание 3
-
-
-    let myDate = new Date;
     let interval = setInterval(() => console.log(new Date), 3000);
     setTimeout(() => { clearInterval(interval); console.log('30 секунд прошло')}, 30000);
 
@@ -84,29 +57,15 @@ function isPositive(number) {
 
     //задание 5
     
-    
-// Функция delayForSecond через 1 секунду пишет в консоль «Прошла одна секунда», 
-// а затем вызывает переданный колбэк
 function delayForSecond(cb) {
     setTimeout(() => {
         console.log('Прошла одна секунда');
-				if(cb) { 	cb(); }
+		if(cb) { 	cb(); }
 
     }, 1000)
 }
-
-// Функция sayHi выводит в консоль приветствие для указанного имени
 function sayHi (name) {
     console.log(`Привет, ${name}!`);
 }
+delayForSecond(() => sayHi('Глеб'));
 
-function nearestSq(n){
-  nearestSq = Math.pow(Math.round(Math.sqrt(n)), 2);
-  }
-  console.log(nearestSq(117));
-
-  function nearestSq(n){
-    nearestSq = Math.pow(Math.round(Math.sqrt(n)), 2);
-    return (n);
-    }
-    console.log(nearestSq(117));
